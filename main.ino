72dbcf5eb67d0d2e7c5504d7863075c8d2462362
David Carl <DavidCarl@users.noreply.github.com>
2016-09-01 14:01:32 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -3,8 +3,19 @@
 // Initialize the library with the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-byte manJumpRight[8] = {
+byte manStand[8] = {
   B01110,
+  B01110,
+  B00100,
+  B11111,
+  B00100,
+  B00100,
+  B01010,
+  B10001
+};
+
+byte manJumpRight[8] = {
+  B01100,
   B01110,
   B00100,
   B11111,
@@ -15,7 +26,7 @@ byte manJumpRight[8] = {
 };  
 
 byte manJumpLeft[8] = {
-  B01110,
+  B00110,
   B01110,
   B00100,
   B11111,
@@ -26,7 +37,7 @@ byte manJumpLeft[8] = {
 };  
 
 byte manRunRightOne[8] = {
-  B01110,
+  B01100,
   B01110,
   B00101,
   B11111,
@@ -36,36 +47,93 @@ byte manRunRightOne[8] = {
   B00001
 };
 byte manRunRightTwo[8] = {
-  B01110,
+  B01100,
   B01110,
   B10100,
   B11111,
   B00100,
-  B00111,
-  B11101,
-  B00001
-};  
+  B00100,
+  B01010,
+  B10001
+};
+
+byte manRunLeftOne[8] = {
+  B00110,
+  B01110,
+  B10100,
+  B11111,
+  B00101,
+  B11100,
+  B10111,
+  B10000
+};
+byte manRunLeftTwo[8] = {
+  B00110,
+  B01110,
+  B00101,
+  B11111,
+  B00100,
+  B00100,
+  B01010,
+  B10001
+};
 
 int spacer = 0;
+int left = 8;
+int jump = 9;
+int right = 10;
+
 
 void setup() {
 // set up the LCD's number of columns and rows
+Serial.begin(9600);
+
+
 lcd.begin(16, 2);
+pinMode(left, INPUT_PULLUP);
+pinMode(jump, INPUT_PULLUP);
+pinMode(right, INPUT_PULLUP);
 //lcd.createChar(0, manJumpRight);
 //lcd.createChar(1, manJumpLeft);
 lcd.createChar(0, manRunRightOne);
 lcd.createChar(1, manRunRightTwo);
+lcd.createChar(2, manRunLeftOne);
+lcd.createChar(3, manRunLeftTwo);
+lcd.createChar(4, manStand);
 }
 
 
 void loop() {
-// Print a message to the LCD
-  lcd.setCursor(spacer,1);
-  lcd.write(byte(spacer%2));
-  delay(200);
-  lcd.clear();
-  spacer++;
-  if(spacer>16){
-    spacer = 0;
+  int leftValue = digitalRead(left);
+  int jumpValue = digitalRead(jump);
+  int rightValue = digitalRead(right);
+  if(leftValue == 0 && rightValue == 1){
+  move(-1,2);
   }
+  if(leftValue == 1 && rightValue == 0) {
+    move(1,0);
+    } 
+    if((leftValue == 1 && rightValue == 1) || (leftValue == 0 && rightValue == 0)){
+      stand();
+      }
+}
+
+void stand(){
+  lcd.setCursor(spacer,1);
+  lcd.write(byte(4));
+}
+
+void move(int into,int add) {
+  Serial.print(into);
+  lcd.setCursor(spacer,1);
+  lcd.write(byte(spacer%2+add));
+  delay(150);
+  lcd.setCursor(spacer,1);
+  lcd.write(' ');
+  spacer+= into;
+  if(spacer>=16){
+    spacer = 0;
+  }else if(spacer<=0){
+    spacer = 16;
+}
 }
